b1606709f919208a27587a11aa135822e8df5ffc
daoqi <daoqidq@gmail.com>
2023-01-31 15:19:09 -0500

    add aws-efs-csi-driver and cluster autoscaler, remove wordpress

--- eks-terraform/helm_deployment.tf
+++ eks-terraform/helm_deployment.tf
@@ -9,7 +9,7 @@ resource "null_resource" "kubectl" {
 ###############################################################
 resource "helm_release" "aws-ebs-csi-driver" {
   name       = "aws-ebs-csi-driver"
-  namespace = "kube-system"
+  namespace  = "kube-system"
 
 
   repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
@@ -20,10 +20,34 @@ resource "helm_release" "aws-ebs-csi-driver" {
     value = "false"
   }
   set{
-    name = "controller.serviceAccount.name"
+    name  = "controller.serviceAccount.name"
     value = "${kubernetes_service_account_v1.ebs-csi-controller.metadata.0.name}"
   }
 }
+
+###############################################################
+# aws-efs-csi-driver
+###############################################################
+
+resource "helm_release" "aws-efs-csi-driver" {
+  name       = "aws-efs-csi-driver"
+  namespace  = "kube-system"
+
+
+  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver"
+  chart      = "aws-efs-csi-driver"
+
+  set {
+    name  = "controller.serviceAccount.create"
+    value = "false"
+  }
+  set{
+    name  = "controller.serviceAccount.name"
+    value = "${kubernetes_service_account_v1.efs-csi-controller.metadata.0.name}"
+  }
+}
+
+
 
 ###############################################################
 # kube-ops-view
@@ -45,9 +69,43 @@ resource "helm_release" "kube-ops-view" {
 ###############################################################
 resource "helm_release" "eks-metrics-server" {
   name       = "eks-metrics-server"
-  namespace = "kube-system"
+  namespace  = "kube-system"
 
   repository = "https://kubernetes-sigs.github.io/metrics-server/"
   chart      = "metrics-server"
 
+}
+
+###############################################################
+# Cluster Autoscaler
+###############################################################
+resource "helm_release" "cluster-autoscaler" {
+  name       = "cluster-autoscaler"
+  namespace  = "kube-system"
+
+  repository = "https://kubernetes.github.io/autoscaler"
+  chart      = "cluster-autoscaler"
+
+  set {
+    name  = "cloudProvider"
+    value = "aws"
+  }
+
+  set {
+    name  = "autoDiscovery.clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "awsRegion"
+    value = var.region
+  }
+  set {
+    name  = "rbac.serviceAccount.create"
+    value = "false"
+  }
+  set {
+    name  = "rbac.serviceAccount.name"
+    value = "${kubernetes_service_account_v1.irsa-cluster-autoscaler.metadata.0.name}"
+  }
 }
--- eks-terraform/irsa.tf
+++ eks-terraform/irsa.tf
@@ -38,6 +38,61 @@ resource "kubernetes_secret_v1" "ebs-csi-controller" {
 
 }
 
+# Service Account for AWS EFS driver
+resource "kubernetes_service_account_v1" "efs-csi-controller" {
+  metadata {
+    name = "efs-csi-controller-irsa"
+    namespace = "kube-system"
+    annotations = {"eks.amazonaws.com/role-arn" = "${module.iam_eks_role_efs.iam_role_arn}"}
+  }
+  secret {
+    name = "${kubernetes_secret_v1.efs-csi-controller.metadata.0.name}"
+  }
+  
+}
+
+resource "kubernetes_secret_v1" "efs-csi-controller" {
+  metadata {
+    name = "efs-csi-controller-irsa"
+    namespace = "kube-system"
+    annotations = {
+      "kubernetes.io/service-account.name" = "efs-csi-controller-irsa"
+    }
+  }
+  
+  # apply again if secret is not created at the first time
+  type = "kubernetes.io/service-account-token"
+  wait_for_service_account_token = false
+
+}
+
+# Service Account for Cluster-Autoscaler
+resource "kubernetes_service_account_v1" "irsa-cluster-autoscaler" {
+  metadata {
+    name = "cluster-autoscaler-irsa"
+    namespace = "kube-system"
+    annotations = {"eks.amazonaws.com/role-arn" = "${module.iam_eks_role_cluster_autoscaler.iam_role_arn}"}
+  }
+  secret {
+    name = "${kubernetes_secret_v1.cluster-autoscaler.metadata.0.name}"
+  }
+}
+
+#
+resource "kubernetes_secret_v1" "cluster-autoscaler" {
+  metadata {
+    name = "cluster-autoscaler-irsa-secret"
+    namespace = "kube-system"
+    annotations = {
+      "kubernetes.io/service-account.name" = "cluster-autoscaler-irsa"
+    }
+  }
+  
+  # apply again if secret is not created at the first time
+  type = "kubernetes.io/service-account-token"
+  wait_for_service_account_token = false
+
+}
 
 # ##############################################################
 # # Create/Link IAM role with service account
@@ -52,6 +107,40 @@ module "iam_eks_role_ebs" {
     one = {
       provider_arn               = module.eks.oidc_provider_arn
       namespace_service_accounts = ["kube-system:ebs-csi-controller-irsa"]
+    }
+  }
+
+  tags = local.tags
+}
+
+module "iam_eks_role_efs" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name = "efs-csi-irsa-${module.eks.cluster_name}"
+
+  attach_efs_csi_policy = true
+
+  oidc_providers = {
+    one = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:efs-csi-controller-irsa"]
+    }
+  }
+
+  tags = local.tags
+}
+
+
+module "iam_eks_role_cluster_autoscaler" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name = "cluster-autoscaler-irsa-${module.eks.cluster_name}"
+
+  attach_cluster_autoscaler_policy = true
+  cluster_autoscaler_cluster_ids = [module.eks.cluster_name]
+
+  oidc_providers = {
+    one = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:cluster-autoscaler-irsa"]
     }
   }
 
